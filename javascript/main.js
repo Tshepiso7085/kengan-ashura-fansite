3e4f2b00bb57f4956bf3c145abce9215bde37add
Tshepiso Masilela <Tshepisomasilela926@gmail.com>
2024-07-07 16:38:00 +0200

    Update main.js

--- javascript/main.js
+++ javascript/main.js
@@ -1,34 +1,39 @@
-class VideoPlayer{
-  constructor(){
+class VideoPlayer {
+  constructor() {
     this.videoPlayer = document.getElementById('video-player');
     this.videoTitle = document.querySelector('.video-description .title');
   }
 
-  async playvideo(videoId){
-    try{
+  async playVideo(videoId) {
+    try {
       const response = await fetch('video.json');
       const videoData = await response.json();
       const video = videoData.find(video => video['id'] === parseInt(videoId));
+
+      if (!video) {
+        throw new Error(`Video with ID ${videoId} not found`);
+      }
+
       const videoUrl = video.url;
       const videoTitle = video.title;
-      console.log(this.videoPlayer);
-      const sourceElement = this.videoPlayer.querySelector('source');
-      sourceElement.src = videoUrl;
+
+      this.videoPlayer.src = videoUrl;
       this.videoPlayer.load();
       this.videoPlayer.play();
       this.videoTitle.textContent = videoTitle;
-    } catch(error){
+    } catch (error) {
       console.error(error);
     }
   }
 }
 
 const videoPlayer = new VideoPlayer();
+
 const buttons = document.querySelectorAll('.button-play');
 
-buttons.forEach(button =>{
+buttons.forEach(button => {
   button.addEventListener('click', () => {
     const videoId = button.getAttribute('data-id');
-    videoPlayer.playvideo(videoId);
-  })
-})
+    videoPlayer.playVideo(videoId);
+  });
+});
